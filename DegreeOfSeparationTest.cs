c55136cf9c0ba4ceda4c5dfcd6dd4d26331e11dc
Nick Rubenstein <nrubenstein24@gmail.com>
2012-11-20 11:01:06 -0600

    Added main algorithm and test data
    Edited the README

---
+++ DegreeOfSeparationTest.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+
+
+class Actor
+{
+    public string name;
+    public List<Film> films = new List<Film>();
+    public bool visited = false;
+    public Film connect = null;
+    public Actor(string n = "none", List<Film> f = null, bool v = false)
+    {
+        name = n;
+        films = f;
+        visited = v;
+    }
+
+    public int Path(Actor actor)        //works backwards and dumps the path into a stack to display it later in the proper order
+    {
+        int degree = 1;
+        Stack<Actor> a = new Stack<Actor>();
+        Stack<Film> f = new Stack<Film>();
+        Film temp = this.connect;
+        a.Push(this);
+        if (temp.connect == actor)      //this is the case if there is only one degree of separation
+        {
+            f.Push(temp);
+            a.Push(actor);
+        }
+        else
+        {
+            while (temp.connect != actor)   //else go through path 
+            {
+                f.Push(temp);
+                a.Push(temp.connect);
+                temp = temp.connect.connect;
+            }
+            f.Push(temp);
+            a.Push(temp.connect);
+        }
+        degree = f.Count;
+        while (a.Count > 1)             //display the path
+        {
+            Console.WriteLine(a.Pop().name);
+            Console.WriteLine("|\n|");
+            Console.WriteLine(f.Pop().name);
+            Console.WriteLine("|\n|");
+        }
+        Console.WriteLine(a.Pop().name);
+        Console.WriteLine("degree of separation: " + degree);//show the degree of separation
+        return degree;
+    }
+
+    public int DegreeOfSeparation(Actor actor, int degree)
+    {
+        List<Actor> q = new List<Actor>();
+        q.Add(this);
+        while (q.Count > 0)
+        {
+            foreach (Film film in q[0].films)   //go through each film of this actor
+            {
+                if (film.visited == false)  //checks if film has been checked already
+                {
+                    film.visited = true;
+                    film.connect = q[0];
+                    foreach (Actor person in film.cast)     //Goes through the cast of each film
+                    {
+                        if (person.visited == false)    //checks if actor has been visited yet
+                        {
+                            person.connect = film;
+                            person.visited = true;
+                            if (person.name == actor.name)     //if actor connection found then use Path() to display path of separation
+                            {
+                                person.Path(this);
+
+                                return 1;
+
+                            }
+                            q.Add(person);
+                        }
+                    }
+                }
+            }
+
+            q.RemoveAt(0);
+        }
+        return -1;
+    }
+
+}
+
+class Film
+{
+    public string name;
+    public List<Actor> cast = new List<Actor>();
+    public bool visited = false;
+    public Actor connect = null;
+    public Film(string n = "none", List<Actor> c = null, bool v = false)
+    {
+        name = n;
+        cast = c;
+        visited = v;
+    }
+
+}
+
+
+namespace _470_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //takes our sample file and indexes it into film and actor objects
+            const string f = "smallactors.txt";
+            List<Film> films = new List<Film>();
+            List<Actor> actors = new List<Actor>();
+            using (StreamReader r = new StreamReader(f))
+            {
+                string line;
+                bool newactor = true;
+                Actor currentActor = new Actor();
+                Film currentFilm = new Film();
+
+                while ((line = r.ReadLine()) != null)
+                {
+                    if (line == "")
+                    {
+                        newactor = true;
+                        continue;
+                    }
+
+                    if (newactor == true)
+                    {
+                        currentActor = new Actor(line, new List<Film>());
+                        actors.Add(currentActor);
+                        newactor = false;
+                    }
+                    else
+                    {
+                        currentFilm = films.Find(delegate(Film film) { return film.name.Equals(line); });
+                        if (currentFilm == null)
+                        {
+                            currentFilm = new Film(line, new List<Actor>());
+                            films.Add(currentFilm);
+                        }
+
+                        currentFilm.cast.Add(currentActor);
+                        currentActor.films.Add(currentFilm);
+                    }
+                }
+
+                //foreach (Film film in films)
+                //{
+                //    Console.WriteLine(film.name);
+                //    foreach (Actor actor in film.cast)
+                //        Console.WriteLine(actor.name);
+                //    Console.ReadLine();
+
+                //}
+                int x = 0;
+                foreach (Actor actor in actors)
+                {
+
+                    Console.WriteLine(x + ".)" + actor.name);
+                    x++;
+                    //foreach (Film film in actor.films)
+                    //Console.WriteLine(film.name);
+                    //Console.ReadLine();
+                }
+                //USER INTERFACE
+                string input1;
+                string input2;
+                Console.WriteLine("Select the first actor by it's corresponding number");
+                input1 = Console.ReadLine();
+                Console.WriteLine("Select the second actor by it's corresponding number");
+                input2 = Console.ReadLine();
+
+                int actor1 = int.Parse(input1);
+                int actor2 = int.Parse(input2);
+                if (actors[actor1].DegreeOfSeparation(actors[actor2], 1) != -1)
+                {
+
+                    Console.WriteLine("good");
+                }
+                else
+                    Console.WriteLine("bad");
+
+                Console.ReadLine();
+            }
+
+
+
+        }
+    }
+}
--- README.txt
+++ README.txt
@@ -3,7 +3,15 @@ By: Nick Rubenstein and Stephen Dawkins
 
 All code is in C# and can be ran in Visual Studio 2010.
 
+The .list files from IMDB are too large to go into the GitHub, so ActorsFileParser.cs cannot be ran
+
 ActorsFileParser.cs		Takes the .list files from the IMDB repo and converts them into .txt files we can use easier.
 				Note: TV shows and movies are removed as well as any actor that only appears in TV show or movies
 
 LoadActorFilmsInMemory.cs	Reads in files in the format the ActorsFileParser.cs outputs to store in memory.
+
+DegreeOfSeparationTest.cs	Using a sample of 10 famous actors, this file tests our main algorithm of assigning degrees of 
+				separtaions from one actor to another
+				Note: the smallactors.txt file must be in the same directory as the program file
+
+smallactors.txt			Text document of the top 10 most famous actors used for testing
